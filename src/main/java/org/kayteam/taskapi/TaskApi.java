ce647c9c2ef7f20485024fb1335125607409dea9
SirOswaldo <DeveloperOswaldo@gmail.com>
2022-09-01 04:27:12 -0300

    TaskApi.java updated

--- src/main/java/org/kayteam/taskapi/TaskApi.java
+++ src/main/java/org/kayteam/taskapi/TaskApi.java
@@ -6,14 +6,12 @@ public final class TaskApi extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
-    }
 
+    }
 
 }
