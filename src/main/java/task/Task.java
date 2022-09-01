8adc41a75e10bac9894b95c16eec0d544242b6c1
SoyAldo <DeveloperOswaldo@gmail.com>
2022-09-01 04:36:48 -0300

    Task.java updated

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,7 +1,37 @@
 package task;
 
-public class Task {
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.scheduler.BukkitTask;
 
+public abstract class Task{
 
+    private final JavaPlugin javaPlugin;
+    private BukkitTask bukkitTask;
+    private final long ticks;
+
+    public Task(JavaPlugin javaPlugin, long ticks){
+        this.javaPlugin = javaPlugin;
+        this.ticks = ticks;
+    }
+
+    public void startScheduler() {
+        BukkitScheduler scheduler = javaPlugin.getServer().getScheduler();
+        bukkitTask = scheduler.runTaskTimer(javaPlugin, this::actions, 0L, ticks);
+    }
+
+    public void stopScheduler(){
+        BukkitScheduler scheduler = javaPlugin.getServer().getScheduler();
+        scheduler.cancelTask(bukkitTask.getTaskId());
+        stopActions();
+    }
+
+    public abstract void actions();
+
+    public void stopActions(){}
+
+    public boolean isRunning() {
+        return javaPlugin.getServer().getScheduler().isCurrentlyRunning(bukkitTask.getTaskId());
+    }
 
 }
