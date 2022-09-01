10296c59f7f92c7e600ef909abde7a611f3743e6
SirOswaldo <DeveloperOswaldo@gmail.com>
2022-09-01 02:19:58 -0300

    Initial commit

---
+++ src/main/java/org/kayteam/taskapi/TaskApi.java
@@ -0,0 +1,17 @@
+package org.kayteam.taskapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TaskApi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
