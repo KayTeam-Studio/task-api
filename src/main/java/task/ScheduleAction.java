fc81604e492d57d257c64b6f0412a9e694e8bfb7
SoyAldo <DeveloperOswaldo@gmail.com>
2022-09-01 04:54:04 -0300

    ScheduleAction.java added and updated

---
+++ src/main/java/task/ScheduleAction.java
@@ -0,0 +1,13 @@
+package task;
+
+import org.bukkit.scheduler.BukkitTask;
+
+public interface ScheduleAction {
+
+    /**
+     * This action can be cancel task if this return true
+     * @return Cancel task if true
+     */
+    boolean action(BukkitTask bukkitTask);
+
+}
